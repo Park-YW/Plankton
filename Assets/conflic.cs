d0a693bf5136b9f0743b1391b9ae787457eb3ba8
seong-ho-y <seonghoy88@gmail.com>
2024-10-02 22:29:28 +0900

    conflic script 수정

--- Assets/conflic.cs
+++ Assets/conflic.cs
@@ -7,7 +7,7 @@ public class conflic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Conflict");
     }
 
     // Update is called once per frame
