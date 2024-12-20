4d790db2b5591f56ffa79091119ac5ddd1e27aec
Park-YW <snake010807@khu.ac.kr>
2024-12-20 13:27:35 +0900

    조합시스템 골조 완료

--- Assets/PYW/Forge.cs
+++ Assets/PYW/Forge.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct CraftingList
+public class CraftingList
 {
     public string make;
     public Dictionary<string, int> needs;
+    public string blueprint;
+    public CraftingList(string make, Dictionary<string, int> needs, string blueprint)
+    {
+        this.make = make;
+        this.needs = needs;
+        this.blueprint = blueprint;
+    }
 }
 public enum ItemCodeList { Dirt, Wood, Rock, Iron, Rubber, Titanium, Gear, Lever }
 
@@ -13,54 +20,70 @@ public class Forge : MonoBehaviour
 {
     public ForgePanel panel;
     public bool _isEntering = false;
-    public List<CraftingList> ListToMake;
+    public CraftingList[] ListToMake;
     int _currentNumber = 0;
 
     private void Awake()
     {  
         panel = GetComponentInChildren<ForgePanel>();
         panel.gameObject.SetActive(false);
+        ListToMake = new CraftingList[5];
+        Dictionary<string, int> dictemp = new Dictionary<string, int>();
+        dictemp.Add("µ¹", 10);
+        dictemp.Add("³ª¹«", 20);
+        CraftingList temp = new CraftingList("µå¸±ÆÈ", dictemp, "µå¸±ÆÈ ¼³°èµµ");
+        ListToMake[0] = temp;
+        dictemp = new Dictionary<string, int>();
+        dictemp.Add("Ã¶", 10);
+        dictemp.Add("Èë", 30);
+        dictemp.Add("°í¹«", 10);
+        temp = new CraftingList("ºÎ½ºÅÍ", dictemp, "ºÎ½ºÅÍ ¼³°èµµ");
+        ListToMake[1] = temp;
+
+        dictemp = new Dictionary<string, int>();
+        dictemp.Add("Ã¶", 20);
+        dictemp.Add("µ¹", 20);
+        dictemp.Add("³ª¹«", 10);
+        temp = new CraftingList("µå¸±ÆÈ+", dictemp, "µå¸±ÆÈ+ ¼³°èµµ");
+        ListToMake[2] = temp;
+
+        dictemp = new Dictionary<string, int>();
+        dictemp.Add("Ã¶", 30);
+        dictemp.Add("Æ¼Å¸´½", 5);
+        dictemp.Add("°í¹«", 30);
+        temp = new CraftingList("Á¦Æ®ÆÑ", dictemp, "Á¦Æ®ÆÑ ¼³°èµµ");
+        ListToMake[3] = temp;
     }
 
     private void Start()
     {
-        CraftingList temp = new CraftingList();
-        temp.needs.Add("µ¹", 10);
-        temp.needs.Add("³ª¹«", 20);
-        temp.make = "µå¸±ÆÈ";
-        ListToMake.Add(temp);
-        temp = new CraftingList();
-        temp.needs.Add("Ã¶", 10);
-        temp.needs.Add("Èë", 30);
-        temp.needs.Add("°í¹«", 10);
-        temp.make = "ºÎ½ºÅÍ";
-        ListToMake.Add(temp);
-        temp = new CraftingList();
-        temp.needs.Add("Ã¶", 20);
-        temp.needs.Add("µ¹", 20);
-        temp.needs.Add("³ª¹«", 10);
-        temp.make = "µå¸±ÆÈ+";
-        ListToMake.Add(temp);
-        temp = new CraftingList();
-        temp.needs.Add("Ã¶", 30);
-        temp.needs.Add("Å¸Å¸´½", 5);
-        temp.needs.Add("°í¹«", 30);
-        temp.make = "Á¦Æ®ÆÑ";
-        ListToMake.Add(temp);
-        temp = new CraftingList();
-        temp.needs.Add("¿À·¡µÈ Åé´Ï¹ÙÄû", 1);
-        temp.needs.Add("¿À·¡µÈ ±â°èÁ¶°¢", 1);
-        temp.needs.Add("³ì½¼ Åé´Ï¹ÙÄû", 1);
-        temp.needs.Add("³ì½¼ ·¹¹ö", 1);
-        temp.make = "¿¤·¹º£ÀÌÅÍ";
-        ListToMake.Add(temp);
+        
     }
 
 
 // Update is called once per frame
-void Update()
+    public void Craft()
     {
 
+        ResourceManager.Instance.CraftItem(ListToMake[_currentNumber].blueprint, ListToMake[_currentNumber].needs, ListToMake[_currentNumber].make);
+    }
+
+    public void ChangeCraftList()
+    {
+        _currentNumber++;
+        _currentNumber %= 4;
+    }
+
+    public List<string> GetCraftList()
+    {
+        List<string> list = new List<string>();
+        list.Add(ListToMake[_currentNumber].blueprint);
+        foreach(var need in ListToMake[_currentNumber].needs)
+        {
+            list.Add(need.Key);
+        }
+
+        return list;
     }
 
     PlayerMovement ff;
